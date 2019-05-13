ab4c0dbd80a4f40ad58472fe8914f5fb31f99765
Szymon Baczyński <simon.baczynski@gmail.com>
2019-05-13 21:25:37 +0200

    End Warmup

--- HackerRank_ProblemsSolving_Algorithms/Algorithms_Warmup.py
+++ HackerRank_ProblemsSolving_Algorithms/Algorithms_Warmup.py
@@ -114,41 +114,40 @@ print("-------------------------------------------------------------------------
 # You can find this on: HackerRank -> Challenges (Tutorials) -> Problem Solving (Algorithms)
 # This is: Problem Solving (Algorithms) - Plus Minus
 
-print("---------- Task 5 ----------")
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the plusMinus function below.
-def plusMinus(arr):
-    positive = 0.0
-    negative = 0.0
-    zeros = 0.0
-
-    for i in arr:
-        if i > 0:
-            positive += 1
-        elif i < 0:
-            negative += 1
-        else:
-            zeros += 1
-
-    m = len(arr)
-
-    print(round(positive/m,6))
-    print(round(negative/m,6))
-    print(round(zeros/m,6))
-
-
-if __name__ == '__main__':
-    # n = 6
-    # arr = [-4, 3, -9, 0, 4, 1]
-    n = int(input())
-    arr = list(map(int, input().rstrip().split()))
-    plusMinus(arr)
+# print("---------- Task 5 ----------")
+#
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# # Complete the plusMinus function below.
+# def plusMinus(arr):
+#     positive = 0.0
+#     negative = 0.0
+#     zeros = 0.0
+#
+#     for i in arr:
+#         if i > 0:
+#             positive += 1
+#         elif i < 0:
+#             negative += 1
+#         else:
+#             zeros += 1
+#
+#     m = len(arr)
+#     print(round(positive/m,6))
+#     print(round(negative/m,6))
+#     print(round(zeros/m,6))
+#
+#
+# if __name__ == '__main__':
+#     # n = 6
+#     # arr = [-4, 3, -9, 0, 4, 1]
+#     n = int(input())
+#     arr = list(map(int, input().rstrip().split()))
+#     plusMinus(arr)
 
 
 print("-----------------------------------------------------------------------------------")
@@ -156,6 +155,164 @@ print("-------------------------------------------------------------------------
 # You can find this on: HackerRank -> Challenges (Tutorials) -> Problem Solving (Algorithms)
 # This is: Problem Solving (Algorithms) - Staircase
 
-print("---------- Task 6 ----------")
+# print("---------- Task 6 ----------")
+#
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# # Complete the staircase function below.
+# def staircase(n):
+#     for i in range(n):
+#         stair = ' ' * (n-i-1)
+#         step = '#' * (i+1)
+#         print(stair + step)
+#
+# if __name__ == '__main__':
+#     n = int(input())
+#     staircase(n)
 
 
+print("-----------------------------------------------------------------------------------")
+# Problem Solving Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> Problem Solving (Algorithms)
+# This is: Problem Solving (Algorithms) - Mini-Max Sum
+
+# print("---------- Task 7 ----------")
+#
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# # Complete the miniMaxSum function below.
+# def miniMaxSum(arr):
+#     kapa = [None]*(len(arr))
+#     suma = sum(arr)
+#
+#     for i in range(0,len(arr)):
+#         kapa[i] = suma - arr[i]
+#         #print(kapa[i])
+#         #print("--- " + str(arr[i]))
+#     minimum = min(kapa)
+#     maximum = max(kapa)
+#     print(str(minimum) + "  " + str(maximum))
+#
+# if __name__ == '__main__':
+#     # arr = [1,2,3,4,5]
+#     arr = list(map(int, input().rstrip().split()))
+#     miniMaxSum(arr)
+
+
+print("-----------------------------------------------------------------------------------")
+# Problem Solving Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> Problem Solving (Algorithms)
+# This is: Problem Solving (Algorithms) - Birthday Cake Candles
+
+# print("---------- Task 8 ----------")
+#
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# # Complete the birthdayCakeCandles function below.
+# def birthdayCakeCandles(ar):
+#     max_element = max(ar)
+#     count = ar.count(max_element)
+#     return count
+#
+# if __name__ == '__main__':
+#     ar_count = 4
+#     ar = [3,2,1,3]
+#     result = birthdayCakeCandles(ar)
+#     print(result)
+#     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     # ar_count = int(input())
+#     # ar = list(map(int, input().rstrip().split()))
+#     # result = birthdayCakeCandles(ar)
+#     # fptr.write(str(result) + '\n')
+#     # fptr.close()
+
+
+print("-----------------------------------------------------------------------------------")
+# Problem Solving Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> Problem Solving (Algorithms)
+# This is: Problem Solving (Algorithms) - Birthday Cake Candles
+
+# print("---------- Task 8 ----------")
+#
+# import os
+# import sys
+#
+# def timeConversion(s):
+#     kapa = s.split(':', 4)
+#     stamp = kapa[2][2:]
+#     kapa[2] = kapa[2].replace('PM','')
+#     kapa[2] = kapa[2].replace('AM','')
+#     if kapa[0] == '12':
+#         kapa[0] = '00'
+#
+#     if stamp == 'PM':
+#         kapa[0] = str(int(kapa[0]) + 12)
+#
+#     time = kapa[0] + ':' + kapa[1] + ':' + kapa[2]
+#     # print(kapa)
+#     # print(stamp)
+#     # print(time)
+#     return time
+#
+#
+# if __name__ == '__main__':
+#     s = "12:05:45PM"
+#
+#     # f = open(os.environ['OUTPUT_PATH'], 'w')
+#     # s = input()
+#     result = timeConversion(s)
+#     print(result)
+#     # f.write(result + '\n')
+#     # f.close()
+
+
+print("-----------------------------------------------------------------------------------")
+# Problem Solving Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> Problem Solving (Algorithms)
+# This is: Problem Solving (Algorithms) - Diagonal Difference
+
+# print("---------- Task 9 ----------")
+#
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# # Complete the diagonalDifference function below.
+# def diagonalDifference(n,arr):
+#     diag1 = 0
+#     diag2 = 0
+#     for i in range(n):
+#         diag1 += arr[i][i]
+#         diag2 += arr[i][(n-1)-i]
+#
+#     diff = abs(diag1 - diag2)
+#     return(diff)
+#
+# if __name__ == '__main__':
+#     n = 3
+#     arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+#     result = diagonalDifference(n, arr)
+#     print(result)
+#
+#     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     # n = int(input())
+#     # arr = []
+#     # for _ in range(n):
+#     #     arr.append(list(map(int, input().rstrip().split())))
+#     # result = diagonalDifference(n,arr)
+#     # fptr.write(str(result) + '\n')
+#     # fptr.close()
