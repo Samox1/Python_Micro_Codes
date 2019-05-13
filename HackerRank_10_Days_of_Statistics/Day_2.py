29731460a29191ac6aeaa7f39e9173e3091db606
Szymon Baczyński <simon.baczynski@gmail.com>
2019-05-13 14:22:21 +0200

    Day 2

---
+++ HackerRank_10_Days_of_Statistics/Day_2.py
@@ -0,0 +1,34 @@
+# Statistics Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 10 Days of Statistics
+# This is: Day 2 - Basic Probability
+
+print("---------- Task 1 ----------")
+
+# In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
+# Answer: 5 / 6
+
+
+# Statistics Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 10 Days of Statistics
+# This is: Day 2 - More Dice
+
+print("---------- Task 2 ----------")
+
+# In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+# Answer: 1 / 9
+
+
+# Statistics Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 10 Days of Statistics
+# This is: Day 2 - Compound Event Probability
+
+print("---------- Task 3 ----------")
+
+# There are 3 urns labeled X, Y, and Z.\
+#     Urn X contains 4 red balls and 3 black balls.
+#     Urn Y contains 5 red balls and 4 black balls.
+#     Urn Z contains 4 red balls and 4 black balls.
+# One ball is drawn from each of the urns. What is the probability that, of the balls drawn, are red and is black?
+# Answer: 17 / 42
+
+
---
+++ HackerRank_10_Days_of_Statistics/Day_3.py
@@ -0,0 +1,5 @@
+# Statistics Training
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 10 Days of Statistics
+# This is: Day 3 - Conditional Probability
+
+print("---------- Task 1 ----------")
