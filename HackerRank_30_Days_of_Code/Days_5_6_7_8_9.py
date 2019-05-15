f349708f43c8f50e42e9056745bb81ece1766c67
Szymon Baczyński <simon.baczynski@gmail.com>
2019-05-16 00:27:24 +0200

    DONE Day 6

--- HackerRank_30_Days_of_Code/Days_5_6_7_8_9.py
+++ HackerRank_30_Days_of_Code/Days_5_6_7_8_9.py
@@ -4,6 +4,27 @@
 
 print("---------- Day 5 ----------")
 
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# if __name__ == '__main__':
+#     n = int(input())
+#
+#     for i in range(1,11):
+#         buf = "%d x %d = %d" % (n, i, n*i)
+#         print(buf)
+
+
+print("-----------------------------------------------------------------------------------")
+# 30 Days of Code
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 30 Days of Code
+# This is: Day 6 - Let's Review
+
+print("---------- Day 6 ----------")
+
 import math
 import os
 import random
@@ -13,9 +34,33 @@ import sys
 if __name__ == '__main__':
     n = int(input())
 
-    for i in range(1,11):
-        buf = "%d x %d = %d" % (n, i, n*i)
-        print(buf)
+    for i in range(0,n):
+        s = input()
+
+    # First Option - Python easy approach
+        s1 = ""
+        s2 = ""
+
+        s1 = s[::2]
+        s2 = s[1::2]
+        print(s1 + " " + s2)
+
+    # Second Option - Typical C/C++ approach
+        s1 = ""
+        s2 = ""
+
+        for index in range(len(s)):
+            if index % 2 == 0:
+                s1 += s[index]
+            else:
+                s2 += s[index]
+
+        print(s1 + " " + s2)
 
 
 print("-----------------------------------------------------------------------------------")
+# 30 Days of Code
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 30 Days of Code
+# This is: Day 7 -
+
+print("---------- Day 7 ----------")
