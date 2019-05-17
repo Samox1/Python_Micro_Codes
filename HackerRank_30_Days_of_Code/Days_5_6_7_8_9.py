b07e4ca519b90365a732d12db20208ecb37292d9
Szymon Baczynski <simon.baczynski@gmail.com>
2019-05-17 15:58:15 +0200

    Day 7

--- HackerRank_30_Days_of_Code/Days_5_6_7_8_9.py
+++ HackerRank_30_Days_of_Code/Days_5_6_7_8_9.py
@@ -25,6 +25,46 @@ print("-------------------------------------------------------------------------
 
 print("---------- Day 6 ----------")
 
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# if __name__ == '__main__':
+#     n = int(input())
+#
+#     for i in range(0,n):
+#         s = input()
+#
+#     # First Option - Python easy approach
+#         s1 = ""
+#         s2 = ""
+#
+#         s1 = s[::2]
+#         s2 = s[1::2]
+#         print(s1 + " " + s2)
+#
+#     # Second Option - Typical C/C++ approach
+#         s1 = ""
+#         s2 = ""
+#
+#         for index in range(len(s)):
+#             if index % 2 == 0:
+#                 s1 += s[index]
+#             else:
+#                 s2 += s[index]
+#
+#         print(s1 + " " + s2)
+
+
+print("-----------------------------------------------------------------------------------")
+# 30 Days of Code
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 30 Days of Code
+# This is: Day 7 - Arrays
+
+print("---------- Day 7 ----------")
+
 import math
 import os
 import random
@@ -34,33 +74,15 @@ import sys
 if __name__ == '__main__':
     n = int(input())
 
-    for i in range(0,n):
-        s = input()
+    arr = list(map(int, input().rstrip().split()))
 
-    # First Option - Python easy approach
-        s1 = ""
-        s2 = ""
-
-        s1 = s[::2]
-        s2 = s[1::2]
-        print(s1 + " " + s2)
-
-    # Second Option - Typical C/C++ approach
-        s1 = ""
-        s2 = ""
-
-        for index in range(len(s)):
-            if index % 2 == 0:
-                s1 += s[index]
-            else:
-                s2 += s[index]
-
-        print(s1 + " " + s2)
+    for i in range(len(arr)):
+        print(arr[len(arr)-i-1], end=" ")
 
 
 print("-----------------------------------------------------------------------------------")
 # 30 Days of Code
 # You can find this on: HackerRank -> Challenges (Tutorials) -> 30 Days of Code
-# This is: Day 7 -
+# This is: Day 8 -
 
-print("---------- Day 7 ----------")
+print("---------- Day 8 ----------")
