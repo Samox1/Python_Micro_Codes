fe67dac0bb72cbcf2a1995115a9d010501f586cb
Szymon Baczyński <simon.baczynski@gmail.com>
2019-05-18 20:18:55 +0200

    Day 9

--- HackerRank_30_Days_of_Code/Days_5_6_7_8_9.py
+++ HackerRank_30_Days_of_Code/Days_5_6_7_8_9.py
@@ -65,19 +65,18 @@ print("-------------------------------------------------------------------------
 
 print("---------- Day 7 ----------")
 
-import math
-import os
-import random
-import re
-import sys
-
-if __name__ == '__main__':
-    n = int(input())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    for i in range(len(arr)):
-        print(arr[len(arr)-i-1], end=" ")
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = list(map(int, input().rstrip().split()))
+#
+#     for i in range(len(arr)):
+#         print(arr[len(arr)-i-1], end=" ")
 
 
 print("-----------------------------------------------------------------------------------")
@@ -86,3 +85,43 @@ print("-------------------------------------------------------------------------
 # This is: Day 8 -
 
 print("---------- Day 8 ----------")
+
+n = int(input())
+kappa = dict
+
+for i in range(n):
+    arr = input().rstrip().split()
+    print(arr)
+    kappa = {arr[0]: int(arr[1])}
+    print(str(kappa))
+
+
+
+print("-----------------------------------------------------------------------------------")
+# 30 Days of Code
+# You can find this on: HackerRank -> Challenges (Tutorials) -> 30 Days of Code
+# This is: Day 9 - Recursion 3
+
+print("---------- Day 9 ----------")
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+# # Complete the factorial function below.
+# def factorial(n):
+#     if n <= 1:
+#         result = 1
+#     else:
+#         result = n * factorial(n-1)
+#     return result
+#
+# if __name__ == '__main__':
+#     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     n = int(input())
+#     result = factorial(n)
+#     print(result)
+#     #fptr.write(str(result) + '\n')
+#     #fptr.close()
