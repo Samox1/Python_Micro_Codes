529bdb8c9101bf68ebde60846d5e185e923b8b9d
Szymon Baczyński <simon.baczynski@gmail.com>
2020-05-17 01:16:17 +0200

    Pipe pracy na Trello

--- CIM_import_program/R_CIM_import.R
+++ CIM_import_program/R_CIM_import.R
@@ -7,7 +7,13 @@ library(xml2)
 
 print("Hello")
 
-data <- xmlParse("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h--SH.xml")
+data <- xmlParse("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h--SH.xml", useInternalNode=TRUE)
 xml_data <- xmlToList(data)
+kappa1 <- xmlToDataFrame(data)
 
-kappa <- read_xml("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h--SH.xml",as_html = FALSE, options = "NOBLANKS")
+kappa <- read_xml("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h--SH.xml",as_html = FALSE)
+
+ObjName <- as.character(xml_data[[2]])
+ObjectNames <- as.character(xml_attrs(xml_child(xml_child(kappa, 2), 2)))
+print(ObjectNames)
+
