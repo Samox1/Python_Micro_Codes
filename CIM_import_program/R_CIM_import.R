05ee1e231ea89a0e89a90e922d7163c39a1cb0c1
Szymon Baczyński <simon.baczynski@gmail.com>
2020-05-19 00:14:50 +0200

    Prawie dziala CIM

--- CIM_import_program/R_CIM_import.R
+++ CIM_import_program/R_CIM_import.R
@@ -1,8 +1,9 @@
 rm(list=ls())
 
 library(tidyverse)
-library(XML)
+library(dplyr)
 library(methods)
+library(XML)
 library(xml2)
 
 print("Hello")
@@ -12,12 +13,61 @@ print("Hello")
 # 2) xmlToList = Lista z danymi i nazwami obiektow - pojawia sie BUG i jest problem z powtarzajacymi sie ID
 # 3) read_xml = Lista z danymi ID 
 
-data <- xmlParse("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h--SH.xml", useInternalNode=TRUE)
-XML_Lista <- xmlToList(data)
-XML_Tabela <- data.frame(xmlToDataFrame(data))
-XML_Lista_ID <- read_xml("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h--SH.xml",as_html = FALSE)
+skok <- 0
+
+Input <- read_lines("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h_Original.xml", skip_empty_rows = FALSE)
+PsrList_ALL <- which(grepl("PowerSystemResource.PsrLists", Input))
+
+for(x in c(1:(length(PsrList_ALL)-2))){
+  if((PsrList_ALL[x]+1) == PsrList_ALL[x+1]){
+    if(((PsrList_ALL[x]+1) == PsrList_ALL[x+1]) & PsrList_ALL[x+1] == (PsrList_ALL[x+2]-1)){
+      # print(c("1 linia: ",Input[PsrList_ALL[x]]))
+      # print(c("2 linia: ",Input[PsrList_ALL[x+1]]))
+      # print(c("3 linia: ",Input[PsrList_ALL[x+2]]))
+      Input[PsrList_ALL[x+1]] <- str_replace_all(Input[PsrList_ALL[x+1]],"PowerSystemResource.PsrLists", "PowerSystemResource.PsrLists2")
+      Input[PsrList_ALL[x+2]] <- str_replace_all(Input[PsrList_ALL[x+2]],"PowerSystemResource.PsrLists", "PowerSystemResource.PsrLists3")
+      # print(c(PsrList_ALL[x], "1 linia: ",Input[PsrList_ALL[x]]))
+      # print(c(PsrList_ALL[x+1], "2 linia: ",Input[PsrList_ALL[x+1]]))
+      # print(c(PsrList_ALL[x+2], "3 linia: ",Input[PsrList_ALL[x+2]]))
+      skok <- 1
+    }else{
+      if(skok == 1){
+        skok <- 0
+        next
+      }
+      # print(c("1 linia: ",Input[PsrList_ALL[x]]))
+      # print(c("2 linia: ",Input[PsrList_ALL[x+1]]))
+      Input[PsrList_ALL[x+1]] <- str_replace_all(Input[PsrList_ALL[x+1]],"PowerSystemResource.PsrLists", "PowerSystemResource.PsrLists2")
+      # print(c("2 mod linia: ",Input[PsrList_ALL[x+1]]))
+      # print(c(PsrList_ALL[x], " linia: ",Input[PsrList_ALL[x]]))
+      # print(c(PsrList_ALL[x+1], " linia: ",Input[PsrList_ALL[x+1]]))
+    }
+  }
+}
+
+print("--- Zmiana PsrLists ---")
+
+#write(Input, "D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h_Moddest.xml")
+# rm(Input)
+# rm(PsrList_ALL)
+# rm(x)
+# Input[189384]
+# Input[189385]
+
+data <- xmlParse(Input, useInternalNode=TRUE)
+print("--- xmlParse ---")
+# data <- xmlParse("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h_HALF.xml", useInternalNode=TRUE)
+# XML_Lista <- xmlToList(data)
+XML_Tabela <- xmlToDataFrame(data)
+print("--- xmlToDataFrame ---")
+XML_Lista_ID <- read_xml("D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_23A10h.xml",as_html = FALSE)
+# XML_Lista_ID <- read_xml(Input,as_html = FALSE)
+print("--- read_xml ---")
+XML_Listowa <- as_list(XML_Lista_ID)
+print("--- as_list(XML_List_ID) ---")
 
 XML_Tabela <- XML_Tabela[, -(1:6)]
+print("--- XML_Tabela okrojona ---")
 
 
 ### 2 - Wyciagniecie nazw obiektow z = xmlToList()
@@ -32,15 +82,19 @@ for (x in c(2:length(XML_Tabela[,1]))) {
 
 XML_Tabela <- data.frame(ObjectsID, XML_Tabela)
 
+print("--- Objects_ID ---")
+
 # Znalezienie nazwy obiektu
 
-ObjectsNames <- names(XML_Lista[1])
+ObjectsNames <- xml_name(xml_child(XML_Lista_ID, 1))
 
 for (x in c(2:length(XML_Tabela[,1]))) {
-  ObjectsNames <- c(ObjectsNames, names(XML_Lista[x]))
+  ObjectsNames <- c(ObjectsNames, xml_name(xml_child(XML_Lista_ID, x)))
 }
 
 XML_Tabela <- data.frame(ObjectsNames, XML_Tabela)
+
+print("--- Objects_Names ---")
 
 
 ### Wpisanie w miejsca NA np. "-"
@@ -48,18 +102,62 @@ XML_Tabela <- data.frame(ObjectsNames, XML_Tabela)
 # XML_Tabela[XML_Tabela == ""] <- "="
 
 
-kappa <- which(grepl("", XML_Tabela$DiscreteValue.Discrete))
-for (x in kappa) {
-  XML_Tabela$DiscreteValue.Discrete[x] <- xml_attrs(xml_child(xml_child(XML_Lista_ID, x), 2))
-}
+# kappa <- which(grepl("", XML_Tabela$DiscreteValue.Discrete))
+# for (x in kappa) {
+#   XML_Tabela$DiscreteValue.Discrete[x] <- xml_attrs(xml_child(xml_child(XML_Lista_ID, x), 2))
+# }
   
 # which(grepl("", XML_Tabela$Equipment.EquipmentContainer))
-kappa1 <- which(grepl("", XML_Tabela$Equipment.EquipmentContainer))
-for (x in kappa1) {
-  XML_Tabela$Equipment.EquipmentContainer[x] <- x
-  print(xml_attrs(xml_child(xml_child(XML_Lista_ID, x), 4))[["resource"]])
+
+# kappa1 <- which(grepl("", XML_Tabela$Equipment.EquipmentContainer))
+# for (x in kappa1) {
+#   XML_Tabela$Equipment.EquipmentContainer[x] <- xml_attrs(xml_child(xml_child(XML_Lista_ID, x), 4))
+# }
+
+### Working BITCH --------------------------------------------------------------------------------------
+# kappa1 <- which(grepl("", XML_Tabela[,8]))
+# for (x in kappa1) {
+#   numeros <- which(grepl(colnames(XML_Tabela[8]), names(XML_Listowa$RDF[[x]])))
+#   XML_Tabela[x,8] <- xml_attrs(xml_child(xml_child(XML_Lista_ID, x), numeros))
+# }
+### ----------------------------------------------------------------------------------------------------
+
+# Od 4 kolumny do konca - sprawdzic czy sa puste4 pola i poszukac atrybutow
+
+for (y in c(4:length(XML_Tabela))) {
+  kappa1 <- which(grepl("^$", XML_Tabela[,y]))
+  if (length(kappa1) > 0){
+    for (x in kappa1) {
+    numeros <- which(grepl(colnames(XML_Tabela[y]), names(XML_Listowa$RDF[[x]])))
+    XML_Tabela[x,y] <- xml_attrs(xml_child(xml_child(XML_Lista_ID, x), numeros))
+    }
+  }
 }
 
+print("--- Uzupe³nianie XML_Tablica - puste miejsca ---")
 
+write_excel_csv(XML_Tabela, "D:/Programming/Python_Micro_Codes/CIM_import_program/MIKRONIKA_ALL_Output.csv")
+
+print("--- Zapis do Excel_CSV ---")
+
+
+### TEST TEST TEST
+
+# xml_name(xml_child(xml_child(XML_Lista_ID, 6), 4))
+
+# xml_length(xml_child(XML_Lista_ID, 6))
+
+# which(grepl("Equipment.EquipmentContainer", names(XML_Listowa$RDF[[47]]))) ### Working - pokazuje index danego elementu obiektu
+
+# kook <- c(1:length(XML_Listowa$RDF))
+# 
+# for (x in c(1:length(XML_Listowa$RDF))) {
+#   kol <- which(grepl("IdentifiedObject.name", names(XML_Listowa$RDF[[kook]])))
+# }
+
+
+
+# numeros <- which(grepl(colnames(XML_Tabela[8]), names(XML_Listowa$RDF[[47]]))) ### Working - pokazuje index danego elementu obiektu w zaleznosci od nazwy kolumny
+# xml_attrs(xml_child(xml_child(XML_Lista_ID, 47), numeros))
 
 
