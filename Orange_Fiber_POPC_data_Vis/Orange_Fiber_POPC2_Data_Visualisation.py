bc33d4257cdbc1c0f2db19ef939f06336462520f
Szymon Baczyński <simon.baczynski@gmail.com>
2020-11-24 00:37:16 +0100

    Scrapy files from Orange website & markers colors

--- Orange_Fiber_POPC_data_Vis/Orange_Fiber_POPC2_Data_Visualisation.py
+++ Orange_Fiber_POPC_data_Vis/Orange_Fiber_POPC2_Data_Visualisation.py
@@ -3,12 +3,33 @@
 
 import pandas as pd
 import folium
+from scrapy import Selector
+import requests
 
 # 1 Step = Import Excel file
 dane = pd.read_excel("lista-punktow-adresowych-popc-nabor-ii-gotowych-sprzedazowo-w-terminie-nie-mniej-niz-30-dni-18.11.2020.xlsx", header=None).iloc[2:, 0:]
 dane = dane.rename(columns = dane.iloc[0,:])
 dane = dane[1:]
 print(dane.head())
+
+orange_server = 'https://www.hurt-orange.pl'
+orange_url = 'https://www.hurt-orange.pl/operatorzy-krajowi/popc-nabor-i-i-ii'
+orange_fiber_site = requests.get(orange_url).content
+sel = Selector(text = orange_fiber_site)
+
+# /html/body/div[3]/div[8]/div/div/div/main/article/div/div/div/div[6]/div/div/div[4]/div/ul/li[29]/a
+#css_test = 'div.tm_pb_attachments_extra:nth-child(4) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(29) > a:nth-child(2)'
+css_test = 'div.tm_pb_attachments_extra:nth-child(4) > div:nth-child(2) > ul:nth-child(2) li a'
+plik = sel.css(css_test).xpath('@href').extract()
+#plik = sel.css(css_test).extract()
+print(plik)                             # Wszystkie linki do plikow na stronie Oragne z adresami do swiatlowodu
+print(len(plik))
+
+# link_pobierania = orange_server + plik[0]
+# print(link_pobierania)
+# dane_z_orange = pd.read_excel(link_pobierania, header=None).iloc[2:, 0:]
+# print(dane_z_orange)
+
 
 # 2 Step = Find coordinates for specific "Gmina"
 jaktorow = dane[dane["Gmina"].str.match("JAKTORÓW")]
@@ -24,7 +45,7 @@ m = folium.Map(location=jaktorow_geo, tiles='OpenStreetMap', zoom_start=15)
 
 # I can add marker one by one on the map
 for i in range(0, len(geodata)):
-    folium.Marker([geodata.iloc[i]['Szerokość'], geodata.iloc[i]['Długość']], popup=geodata.iloc[i,:]).add_to(m)
+    folium.Marker([geodata.iloc[i]['Szerokość'], geodata.iloc[i]['Długość']], popup=geodata.iloc[i,:], icon=folium.Icon(color='green')).add_to(m)
 
 # Save it as html
 m.save('mapka.html')
