3c0d1e1e1c470cfb5911febb57f5e70c76d0c0c2
Szymon Baczyński <simon.baczynski@gmail.com>
2020-11-26 18:33:52 +0100

    Deploy after some changes - @st.rerun and automatic zoom on ROI

--- Orange_Fiber_POPC_data_Vis/Orange_POPC2_Vis-app.py
+++ Orange_Fiber_POPC_data_Vis/Orange_POPC2_Vis-app.py
@@ -4,32 +4,82 @@
 import streamlit as st
 from streamlit_folium import folium_static
 import pandas as pd
+import numpy
 import folium
 from scrapy import Selector
 import requests
+
+
+### 1 - Import Data from Orange website
+
+@st.cache
+def Import_Data_Orange_Fiber():
+    orange_server = 'https://www.hurt-orange.pl'
+    orange_url = 'https://www.hurt-orange.pl/operatorzy-krajowi/popc-nabor-i-i-ii'
+    orange_fiber_site = requests.get(orange_url).content
+    sel = Selector(text = orange_fiber_site)
+    # css_test = 'div.tm_pb_attachments_extra:nth-child(4) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(29) > a:nth-child(2)'
+    css_test = 'div.tm_pb_attachments_extra:nth-child(4) > div:nth-child(2) > ul:nth-child(2) li a'
+    plik = sel.css(css_test).xpath('@href').extract()
+    #print(plik)                                                                 # Wszystkie linki do plikow na stronie Oragne z adresami do swiatlowodu
+    ile = len(plik)
+    #print(ile)
+    print('Extract files names from Orange website')
+
+    # columns=['Nazwa obszaru', 'Identyfikator budynku', 'Województwo', 'Powiat', 'Gmina', 'Kod TERC', 'Miejscowość', 'SIMC', 'Ulica', 'Kod ULIC', 'Nr ', 'Szerokość', 'Długość', 'SFH/MFH', 'Dostępna prędkość [Mb]', 'Liczba lokali']
+
+    dane_z_orange = pd.DataFrame(columns=['Nazwa obszaru', 'Identyfikator budynku', 'Województwo', 'Powiat', 'Gmina', 'Kod TERC', 'Miejscowość', 'SIMC', 'Ulica', 'Kod ULIC', 'Nr ', 'Szerokość', 'Długość', 'SFH/MFH', 'Dostępna prędkość [Mb]', 'Liczba lokali'])
+    for i in range(1,len(plik)):
+        if (plik[i] != '/wp-content/uploads/2020/11/lista-obszarow-i-miejscowosci-objetych-planami-realizacji-orange-w-ramach-ii-konkursu-popc.xlsx' and plik[i] != '/wp-content/uploads/2020/09/lista-punktow-adresowych-ii-konkurs-popc-1.xlsx'):
+            link_pobierania = orange_server + plik[i]
+            print(str(i) + ': ' + link_pobierania)
+            tymczasowa_tablica = pd.read_excel(link_pobierania, header=[2,]).iloc[2:, 0:]
+            dane_z_orange = dane_z_orange.append(tymczasowa_tablica, ignore_index=True, sort=False)
+
+    ### 1.2 Step = Take data and make sure if coordinates are numeric values
+    dane_z_orange = dane_z_orange[['Nazwa obszaru', 'Identyfikator budynku', 'Województwo', 'Powiat', 'Gmina', 'Kod TERC', 'Miejscowość', 'SIMC', 'Ulica', 'Kod ULIC', 'Nr ', 'Szerokość', 'Długość', 'SFH/MFH', 'Dostępna prędkość [Mb]', 'Liczba lokali']]
+    dane_z_orange[["Szerokość", "Długość"]] = dane_z_orange[["Szerokość", "Długość"]].apply(pd.to_numeric, errors='coerce')
+    dane_z_orange = dane_z_orange[dane_z_orange["Szerokość"].notna()]
+    dane_z_orange = dane_z_orange[dane_z_orange["Długość"].notna()]
+
+    Gminy_ALL = dane_z_orange['Gmina'].unique()
+    Gminy_ALL.sort()
+    Gminy_ALL = list(Gminy_ALL)
+    jaktorow_position = Gminy_ALL.index('JAKTORÓW')
+
+    return dane_z_orange, Gminy_ALL, jaktorow_position
+
+print('*** End of Extract Data ***')
+dane_z_orange, Gminy_ALL, jaktorow_position = Import_Data_Orange_Fiber()
+
+### 3.1 - Make Streamlit app
 
 st.write(""" # Wizualizacja danych Orange POPC2 - Fiber To The Home
 Mapa punktów, które zostały lub będą podłączone do sieci światłowodowej """)
 
 st.sidebar.markdown(""" [Strona Orange, z której pochodzą dane (POPC2)] (https://www.hurt-orange.pl/operatorzy-krajowi/popc-nabor-i-i-ii/) """)
 st.sidebar.header('Wybierz gminę:')
-Gmina_All = ['Jaktorów', 'Grodzisk Mazowiecki', 'Brwinów']
-Gmina = st.sidebar.selectbox('Gmina: ', Gmina_All)
-st.sidebar.text('Made by SamoX')
+Gmina = st.sidebar.selectbox('Gmina: ', Gminy_ALL, index=jaktorow_position)
+st.text('\n\n\nMade by SamoX')
+
 # st.sidebar.slider('Do momentu w czasie', ['29-11-2020', '2-12-2020'])
-print(Gmina)
 
 jaktorow_geo = [52.079488, 20.551613]
-# grodzisk_geo = [52.103698, 20.623369]
-# brwinow_geo = [52.142222, 20.719591]
-
-# polaczenie = {}
-# polaczenie['Jaktorów'] = jaktorow_geo
-# polaczenie['Grodzisk Mazowiecki'] = grodzisk_geo
-# polaczenie['Brwinów'] = brwinow_geo
+jaktorow_pkp_geo = [52.086587629803624, 20.55210270975992]
+dane_z_orange_jaktorow = dane_z_orange[dane_z_orange["Gmina"].str.contains(Gmina)]
 
 m = folium.Map(location=jaktorow_geo, tiles='OpenStreetMap', zoom_start=14)
+popup_text_liczba = 'Gmina: ' + Gmina + '\n' + 'Liczba podłączeń = ' + str(len(dane_z_orange_jaktorow))
+folium.Marker([jaktorow_pkp_geo[0], jaktorow_pkp_geo[1]], popup=popup_text_liczba, icon=folium.Icon(color='red', icon='info-sign')).add_to(m)
+
+# I can add marker one by one on the map
+for i in range(0, len(dane_z_orange_jaktorow)):
+    popup_text = 'Miejscowość: ' + str(dane_z_orange_jaktorow.iloc[i]['Miejscowość']) + '\nUlica: ' + str(dane_z_orange_jaktorow.iloc[i]['Ulica']) + ' ' + str(dane_z_orange_jaktorow.iloc[i]['Nr '])
+    folium.Marker([dane_z_orange_jaktorow.iloc[i]['Szerokość'], dane_z_orange_jaktorow.iloc[i]['Długość']], popup=popup_text, icon=folium.Icon(color='green')).add_to(m)
+
 folium_static(m)
+
+
 
 #### 1 Step = Import Excel file from Orange website
 #
