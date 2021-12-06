5732fecfe9a022e466238cc10020a5efc08df726
Szymon Baczyński <simon.baczynski@gmail.com>
2021-12-06 15:02:53 +0100

    R - vol.2 - Zadanie 4.1 - done

--- R_scripts/R_zadanka_vol-2/Zadanie_4/moje-kody_4.r
+++ R_scripts/R_zadanka_vol-2/Zadanie_4/moje-kody_4.r
@@ -1,69 +1,101 @@
+# Plik proszę nazwać numerem swojego indeksu.
+# 
+# Zadanie 1:
+# a) Stwórz funkcję "StopIfNot" przyjmującą nastęujące parametry: "Y", "X", "data", "type", "depth", "minobs", "overfit", "cf".
+# b) Funkcja powinna sprawdzać czy nauka modelu jest możliwa do wykonania, tj:
+#    - czy "data" jest ramką danych,
+#    - czy wszystkie wymienione zmienne ("Y", "X") istnieją w "data",
+#    - czy zmienna "Y" oraz zmienne "X" w tabeli "data" nie ma braków danych,
+#    - czy "depth" oraz "minobs" są większe od 0,
+#    - czy "type" przyjmuje watrtość "Gini", "Entropy", "SS",
+#    - czy "overfit" przyjmuje watrtość "none" lub "prune",
+#    - czy "cf" jest w przedziale (0,0.5],
+#    - czy możliwe kombinacje parametrów mają sens, np. "type = SS" kiedy "Y" jest faktorem.
+# c) W przypadku niespełniania któregoś z warunków, funkcja powinna wyświetlić w konsoli, czego dotyczy problem.
+# d) Funkcja zwraca "TRUE", jeżeli nauka jest możliwa, w przeciwnym wypadku "FALSE". 
+
+
 # Zadanie 1:
 
 StopIfNot <- function(Y, X, data, type, depth, minobs, overfit, cf){
-  #    - czy "data" jest ramkÄ… danych,
-  war1 <- class(data)=="data.frame"
-  #- czy wszystkie wymienione zmienne ("Y", "X") istniejÄ… w "data",
-  war2 <- is.element("X", colnames(data))
-  war3 <- is.element("Y", colnames(data))
-  war4 <- sum(is.na(Y))==0
-  war5 <- sum(is.na(X))==0
-  war6 <- depth >0
-  war7 <- minobs >0
-  war8 <- (type == "Gini" || type == "SS" || type=="Entropy")
-  war9 <- (overfit == "none" || overfit =="prune")
-  war10 <- cf >0 && cf <=0.5
-  war11 <- (class(Y)=="numeric" && type=="SS") || (class(Y)=="factor" && type=="Gini") || (class(Y)=="factor" && type == "Entropy")
+
+  war1 <- is.data.frame(data)
+  war2 <- all(is.element(Y, colnames(data)))
+  war3 <- all(is.element(X, colnames(data)))
   
-  if(war1 == FALSE){
-    cat("data is not a class 'data.frame'")
+  if(war2 && war3)
+  { 
+    war4 <- !any(is.na(data[,Y])) 
+    war5 <- !any(is.na(data[,X]))
+    war11 <- (is.numeric(data[,Y]) && type=="SS") || (is.factor(data[,Y]) && type=="Gini") || (is.factor(data[,Y]) && type == "Entropy")
   }
-  if(war2 == FALSE){
-    cat("'X' is not in data")
-  }
-  if(war3 == FALSE){
-    cat("'y' is not in data")
-  }
-  if(war4 == FALSE){
-    cat("Y ma braki danych")
-  }
-  if(war5 == FALSE){
-    cat("X ma braki danych")
-  }
-  if(war6 == FALSE){
-    cat("depth should be greater than 0")
-  }
-  if(war7 == FALSE){
-    cat("minobs should be greater than 0")
-  }
-  if(war8 == FALSE){
-    cat("type should be 'Gini', 'Entropy' or 'SS'")
-  }
-  if(war9 == FALSE){
-    cat("overfit should be 'none' or 'prune'")
-  }
-  if(war10 == FALSE){
-    cat("cf should be value from (0,0.5]")
-  }
-  if(war11 == FALSE){
-    cat("combination of Y and type parameters does not make sense")
-  }
-  
-  if(war1 && war2 &&war3 && war4 &&war5&&war6 && war7&&war8&&war9&&war10&&war11){
-    return(TRUE)
-  }else{
+  else{
+    warning("Nie ma wskazanych kolumn")
     return(FALSE)
   }
   
+  war6 <- depth > 0
+  war7 <- minobs > 0
+  war8 <- (type == "Gini" || type == "SS" || type == "Entropy")
+  war9 <- (overfit == "none" || overfit =="prune")
+  war10 <- cf > 0 && cf <= 0.5
+  
+  
+  if(war1 == FALSE){
+    warning("ramka danych nie jest typu 'data.frame'")
   }
+  if(war4 == FALSE){
+    warning("kolumny 'Y' -> braki danych")
+  }
+  if(war5 == FALSE){
+    warning("kolumny 'X' -> braki danych")
+  }
+  if(war6 == FALSE){
+    warning("parametr 'depth' powinien byc wiekszy od 0")
+  }
+  if(war7 == FALSE){
+    warning("parametr 'minobs' powinien byc wiekszy od 0")
+  }
+  if(war8 == FALSE){
+    warning("parametr 'type' moze przyjmowac wartosci: 'Gini', 'Entropy', 'SS'")
+  }
+  if(war9 == FALSE){
+    warning("parametr 'overfit' powinien byc: 'none' lub 'prune'")
+  }
+  if(war10 == FALSE){
+    warning("parametr 'cf' zawiera sie w przedziale: (0,0.5]")
+  }
+  if(war11 == FALSE){
+    warning("kombinacja 'Y' i parametru 'type' nie ma sensu")
+  }
+  
+  if(war1 && war2 && war3 && war4 && war5 && war6 && war7 && war8 && war9 && war10 && war11)
+  {
+    return(TRUE)
+  }
+  else
+  {
+    return(FALSE)
+  }
+  
+}
 
-
+# --- Zadanie 1 - test --- #
 data(mtcars)
-colnames(mtcars)[1:2] <- c("X", "Y")
-StopIfNot(X, Y, data=mtcars, type = "SS", depth = -2,minobs = 1,overfit = "nne", cf = 2)
+mtcars
+mtcars[5,"qsec"] <- NA
+StopIfNot("qsec", c("hp","vs","gear"), data=mtcars, type = "SS", depth = -2, minobs = 1, overfit = "nne", cf = 2)
+# --- Zadanie 1 - test --- #
 
 
+
+# Zadanie 2:
+# a) Stwórz funkcję "AssignInitialMeasures" przyjmującą nastęujące parametry: "tree", "Y", "data", "type", "depth".
+# b) Funkcja powinna na podstawie parametrów wejściowych przypisywać do obiektu "tree" (czyli korzenia) wartości początkowe:
+#    - "depth" = 0.
+#    - w zależności od "type" wartość miary Gini, Entropy, SS dla calej populacji (bo to korzeń).
  
+
 # Zadanie 2:
 
 Prob <- function( y ){
@@ -113,6 +145,13 @@ AssignInitialMeasures <- function(tree, Y, data, type, depth){
   return(tree)
 }
   
+
+
+# Zadanie 3:
+# a) Stwórz funkcję "AssignInfo" przyjmującą nastęujące parametry: "tree", "Y", "X", "data", "type", "depth", "minobs", "overfit", "cf".
+# b) Funkcja powinna na podstawie parametrów wejściowych przypisywać do obiektu "tree" (jako attrybuty obiektu) wartości owych parametrów.
+
+
 # Zadanie 3: 
 
 AssignInfo <- function(tree, Y,X, data, type, depth, minobs, overfit, cf){
@@ -126,6 +165,18 @@ AssignInfo <- function(tree, Y,X, data, type, depth, minobs, overfit, cf){
   attr(tree, "cf")  <- cf
   return(tree)
 }
+
+
+
+# Zadanie 4:
+# a) Stwórz funkcję "FindBestSplit" przyjmującą nastęujące parametry: "Y", "X", "data", "parentVal", "type", "minobs".
+# b) Funkcja powinna zwracać tabelę z wynikami najlepszego możliwego podziału, zawierjącą:
+#    - "infGain" - zysk informacyjny dla podziału, 
+#    - "lVal" - miarę niejednorodności dla lewego węzła, 
+#    - "rVal" - miarę niejednorodności dla prawego węzła,
+#    - "point" - punkt (lub zbiór punktów dla zmiennych kategorycznych) podzału,
+#    - "Ln" - liczbę obserwacji w lewym węźle, 
+#    - "Rn" - liczbę obserwacji w prawym węźle.   
 
 
 # Zadanie 4:
@@ -180,6 +231,19 @@ FindBestSplit <- function(Y, X, data, parentVal, type, minobs){
 }
 
 
+
+# Zadanie 5:
+# a) Stwórz funkcję "Tree" przyjmującą nastęujące parametry: "Y", "X", "data", "type", "depth", "minobs", "overfit", "cf".
+# b) Jest to rozwinięcie funkcji ze slajdu nr 19. Funckja powinna po kolei wywoływać pozostałe funkcje:
+#    - "StopIfNot", jeżeli zwracana wartość to "FALSE" to kończymy działanie całej funkcji (zwracamy obiekt niewidzialny),
+#    - tworzenie obiektu "tree",
+#    - "AssignInitialMeasures",
+#    - "BuildTree",
+#    - "PruneTree", na tę chwilę ta funkcja jest pusta PruneTree<-function(){},
+#    - "AssignInfo".
+# c) Funkcja powwina zwracać obiekt "tree".
+
+
 # Zadanie 5:
 
 #PruneTree<-function(){}
@@ -214,7 +278,10 @@ Tree <- function(Y, X, data, type, depth, minobs, overfit, cf){
 
 
 # Zadanie 6:
+# a) Dokonaj integracji opracowanej funkcji "FindBestSplit" z funkcjami "Tree" oraz "BuildTree".
 
+
+# Zadanie 6:
 
 BuildTree <- function( node, Y, Xnames, data,type, depth, minobs ){
   node$Count <- nrow( data )
