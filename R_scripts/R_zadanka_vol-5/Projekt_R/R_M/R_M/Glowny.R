6d41679ee823e06e2b12e8a06ded779e4f20e3a8
Szymon Baczyński <simon.baczynski@gmail.com>
2022-02-10 14:40:01 +0100

    R - Zadania - vol. 5 - skończony raport

--- R_scripts/R_zadanka_vol-5/Projekt_R/R_M/R_M/Glowny.R
+++ R_scripts/R_zadanka_vol-5/Projekt_R/R_M/R_M/Glowny.R
@@ -65,7 +65,7 @@ parTune_Tree_multi <- expand.grid(type = c('Entropy', 'Gini'), depth = c(3,6,9),
 CV_Tree_multi <- CrossValidTune(wieloklasowa_Y, wieloklasowa_X, wieloklasowa, algo = 'Tree', kFold = 10, parTune = parTune_Tree_multi, seed = 333)
 
 
-# KNN - do przeliczenia - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# KNN
 
 parTune_KNN_reg <- expand.grid(k = c(2:15))
 CV_KNN_reg <- CrossValidTune(regresja_Y, regresja_X, regresja, algo = 'KNN', kFold = 10, parTune = parTune_KNN_reg, seed = 333)
@@ -76,7 +76,7 @@ CV_KNN_bin <- CrossValidTune(binarna_Y, binarna_X, binarna, algo = 'KNN', kFold 
 parTune_KNN_multi <- expand.grid(k = c(2:15))
 CV_KNN_multi <- CrossValidTune(wieloklasowa_Y, wieloklasowa_X, wieloklasowa, algo = 'KNN', kFold = 10, parTune = parTune_KNN_multi, seed = 333)
 
-#     - do przeliczenia - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 
 
 
@@ -190,14 +190,14 @@ ggplot(Tree_reg_CrossValid_gr , aes(x=1:nrow(Tree_reg_CrossValid_gr))) +
   geom_line(aes(y = MAET, color='red'), size=1, ) +
   geom_line(aes(y = MAEW, color='blue'), size=1,) +
   labs(title='Tree - Regresja: MAE dla kazdego modelu', x='ID modelu', y='MAE') +
-  scale_color_discrete(name = "Predykcja na zbiorze", labels = c("Treningowym", "Walidacyjnym")) +
+  scale_color_discrete(name = "Predykcja na zbiorze: ", labels = c("Treningowym", "Walidacyjnym")) +
   theme(legend.position = "bottom")
 
 ggplot(Tree_reg_CrossValid_gr , aes(x=1:nrow(Tree_reg_CrossValid_gr))) +
   geom_line(aes(y = MAPET, color='red'), size=1, ) +
   geom_line(aes(y = MAPEW, color='blue'), size=1,) +
   labs(title='Tree - Regresja: MAPE dla kazdego modelu', x='ID modelu', y='MAPE') +
-  scale_color_discrete(name = "Predykcja na zbiorze", labels = c("Treningowym", "Walidacyjnym")) +
+  scale_color_discrete(name = "Predykcja na zbiorze: ", labels = c("Treningowym", "Walidacyjnym")) +
   theme(legend.position = "bottom")
 
 
@@ -239,6 +239,38 @@ ggplot(Tree_multi_CrossValid_gr , aes(x=1:nrow(Tree_multi_CrossValid_gr))) +
 
 
 
+KNN_reg_CrossValid_gr <- CV_KNN_reg %>% group_by(k)
+KNN_reg_CrossValid_gr[is.na(KNN_reg_CrossValid_gr)] <- 0
+KNN_reg_CrossValid_gr <- as.data.frame(KNN_reg_CrossValid_gr %>% summarise(MAET = mean(MAET), MSET = mean(MSET), MAPET = mean(MAPET), MAEW = mean(MAEW), MSEW = mean(MSEW), MAPEW = mean(MAPEW)))
+print(KNN_reg_CrossValid_gr)
+KNN_reg_best_T <- KNN_reg_CrossValid_gr[which.min(KNN_reg_CrossValid_gr$MAET),]
+KNN_reg_best_W <- KNN_reg_CrossValid_gr[which.min(KNN_reg_CrossValid_gr$MAEW),]
+print(KNN_reg_best_T)
+print(KNN_reg_best_W)
+
+
+KNN_bin_CrossValid_gr <- CV_KNN_bin %>% group_by(k)
+KNN_bin_CrossValid_gr[is.na(KNN_bin_CrossValid_gr)] <- 0
+KNN_bin_CrossValid_gr <- as.data.frame(KNN_bin_CrossValid_gr %>% summarise(AUCT = mean(AUCT), CzuloscT = mean(CzuloscT), SpecyficznoscT = mean(SpecyficznoscT), JakoscT = mean(JakoscT),
+                                                                           AUCW = mean(AUCW), CzuloscW = mean(CzuloscW), SpecyficznoscW = mean(SpecyficznoscW), JakoscW = mean(JakoscW), ))
+print(KNN_bin_CrossValid_gr)
+KNN_bin_best_T <- KNN_bin_CrossValid_gr[which.max(KNN_bin_CrossValid_gr$JakoscT),]
+KNN_bin_best_W <- KNN_bin_CrossValid_gr[which.max(KNN_bin_CrossValid_gr$JakoscW),]
+print(KNN_bin_best_T)
+print(KNN_bin_best_W)
+
+
+KNN_multi_CrossValid_gr <- CV_KNN_multi %>% group_by(k)
+KNN_multi_CrossValid_gr[is.na(KNN_multi_CrossValid_gr)] <- 0
+KNN_multi_CrossValid_gr <- as.data.frame(KNN_multi_CrossValid_gr %>% summarise(JakoscT = mean(JakoscT), JakoscW = mean(JakoscW)))
+print(KNN_multi_CrossValid_gr)
+KNN_multi_best_T <- KNN_multi_CrossValid_gr[which.max(KNN_multi_CrossValid_gr$JakoscT),]
+KNN_multi_best_W <- KNN_multi_CrossValid_gr[which.max(KNN_multi_CrossValid_gr$JakoscW),]
+print(KNN_multi_best_T)
+print(KNN_multi_best_W)
+
+
+
 
 
 #######################################################################################################
@@ -247,46 +279,17 @@ ggplot(Tree_multi_CrossValid_gr , aes(x=1:nrow(Tree_multi_CrossValid_gr))) +
 
 cv_R <- trainControl(method="cv", number=10)
 
-# KNN
-binarna_R <- binarna
-binarna_R[,7] <- as.numeric(as.character(as.numeric(binarna_R[,7])))
-knn_grid_bin = expand.grid(k=2:20)
-KNN_bin_R = train(x=binarna_R[,binarna_X], y=binarna_R[,binarna_Y], tuneGrid=knn_grid_bin, method='knn', metric='Accuracy', trControl=cv_R)
-KNN_bin_R_Wynik = KNN_bin_R$results
-print(paste("KNN w R - bin: k = ", KNN_bin_R$finalModel$k, " | Accuracy = " ,KNN_bin_R_Wynik$Accuracy[KNN_bin_R_Wynik$k == KNN_bin_R$finalModel$k]))
-
-knn_grid_multi = expand.grid(k=2:20)
-KNN_multi_R = train(x=wieloklasowa[,wieloklasowa_X], y=wieloklasowa[,wieloklasowa_Y], tuneGrid=knn_grid_multi, method='knn', metric='Accuracy', trControl=cv_R)
-KNN_multi_R_Wynik = KNN_multi_R$results
-print(paste("KNN w R - multi: k = ", KNN_multi_R$finalModel$k, " | Accuracy = " ,KNN_multi_R_Wynik$Accuracy[KNN_multi_R_Wynik$k == KNN_multi_R$finalModel$k]))
-
-regresja_R <- regresja
-regresja_R[,1] <- as.numeric(regresja_R[,1])
-knn_grid_reg = expand.grid(k=2:20)
-KNN_reg_R = train(x=regresja_R[,regresja_X], y=regresja_R[,regresja_Y], tuneGrid=knn_grid_reg, method='knn', metric='MAE', trControl=cv_R)
-KNN_reg_R_Wynik = KNN_reg_R$results
-print(paste("KNN w R - reg: k = ", KNN_reg_R$finalModel$k, " | MAE = " ,KNN_reg_R_Wynik$MAE[KNN_reg_R_Wynik$k == KNN_reg_R$finalModel$k]))
-
-
-# Drzewa decyzyjne
-
-tree_grid_bin = expand.grid(maxdepth=3:9)
-Tree_bin_R = train(x=binarna[,binarna_X], y=binarna[,binarna_Y], tuneGrid=tree_grid_bin, method='rpart2', metric='Accuracy', trControl=cv_R)
-Tree_bin_R_Wynik = Tree_bin_R$results
-print(paste("Drzewo w R - bin: Max Depth = ", Tree_bin_R[["finalModel"]][["tuneValue"]][["maxdepth"]], " | Accuracy = " , Tree_bin_R_Wynik$Accuracy[Tree_bin_R_Wynik$maxdepth == Tree_bin_R[["finalModel"]][["tuneValue"]][["maxdepth"]]]))
-
-tree_grid_multi = expand.grid(maxdepth=3:9)
-Tree_multi_R = train(x=wieloklasowa[,wieloklasowa_X], y=wieloklasowa[,wieloklasowa_Y], tuneGrid=tree_grid_multi, method='rpart2', metric='Accuracy', trControl=cv_R)
-Tree_multi_R_Wynik = Tree_multi_R$results
-print(paste("Drzewo w R - multi: Max Depth = ", Tree_multi_R[["finalModel"]][["tuneValue"]][["maxdepth"]], " | Accuracy = " , Tree_multi_R_Wynik$Accuracy[Tree_multi_R_Wynik$maxdepth == Tree_multi_R[["finalModel"]][["tuneValue"]][["maxdepth"]]]))
-
-tree_grid_reg = expand.grid(maxdepth=3:9)
-Tree_reg_R = train(x=regresja[,regresja_X], y=regresja[,regresja_Y], tuneGrid=tree_grid_reg, method='rpart2', metric='MAE', trControl=cv_R)
-Tree_reg_R_Wynik = Tree_reg_R$results
-print(paste("Drzewo w R - reg: Max Depth = ", Tree_reg_R[["finalModel"]][["tuneValue"]][["maxdepth"]], " | MAE = " , Tree_reg_R_Wynik$MAE[Tree_reg_R_Wynik$maxdepth == Tree_reg_R[["finalModel"]][["tuneValue"]][["maxdepth"]]]))
-
 
 # Sieci neuronowe
+
+regresja_norm <- regresja
+regresja_norm <- data.frame(sapply(regresja, as.numeric))
+regresja_norm <- sapply(regresja_norm, MinMax)
+summary(regresja_norm)
+nn_grid_reg = expand.grid(size=2:10, decay = c(0.0001, 0.01))
+NN_reg_R = train(x=regresja_norm[,regresja_X], y=regresja_norm[,regresja_Y], tuneGrid=nn_grid_reg, method='nnet', metric='MAE', trControl=cv_R)
+NN_reg_R_Wynik = NN_reg_R$results
+print(paste0("Siec NN w R - reg: h = ", NN_reg_R[["finalModel"]][["tuneValue"]][["size"]], " | MAE = " , NN_reg_R_Wynik$MAE[NN_reg_R_Wynik$size == NN_reg_R[["finalModel"]][["tuneValue"]][["size"]]][1]))
 
 binarna_norm <- binarna
 binarna_norm[binarna_X] <- data.frame(sapply(binarna[binarna_X], as.numeric))
@@ -306,14 +309,88 @@ NN_multi_R = train(x=wieloklasowa_norm[,wieloklasowa_X], y=wieloklasowa[,wielokl
 NN_multi_R_Wynik = NN_multi_R$results
 print(paste("Siec NN w R - multi: h = ", NN_multi_R[["finalModel"]][["tuneValue"]][["size"]], " | Accuracy = " , NN_multi_R_Wynik$Accuracy[NN_multi_R_Wynik$size == NN_multi_R[["finalModel"]][["tuneValue"]][["size"]]]))
 
-regresja_norm <- regresja
-regresja_norm <- data.frame(sapply(regresja, as.numeric))
-regresja_norm <- sapply(regresja_norm, MinMax)
-summary(regresja_norm)
-nn_grid_reg = expand.grid(size=2:10, decay = c(0.0001, 0.01))
-NN_reg_R = train(x=regresja_norm[,regresja_X], y=regresja_norm[,regresja_Y], tuneGrid=nn_grid_reg, method='nnet', metric='MAE', trControl=cv_R)
-NN_reg_R_Wynik = NN_reg_R$results
-print(paste0("Siec NN w R - reg: h = ", NN_reg_R[["finalModel"]][["tuneValue"]][["size"]], " | MAE = " , NN_reg_R_Wynik$MAE[NN_reg_R_Wynik$size == NN_reg_R[["finalModel"]][["tuneValue"]][["size"]]][1]))
+
+
+# Drzewa decyzyjne
+
+tree_grid_reg = expand.grid(maxdepth=3:9)
+Tree_reg_R = train(x=regresja[,regresja_X], y=regresja[,regresja_Y], tuneGrid=tree_grid_reg, method='rpart2', metric='MAE', trControl=cv_R)
+Tree_reg_R_Wynik = Tree_reg_R$results
+print(paste("Drzewo w R - reg: Max Depth = ", Tree_reg_R[["finalModel"]][["tuneValue"]][["maxdepth"]], " | MAE = " , Tree_reg_R_Wynik$MAE[Tree_reg_R_Wynik$maxdepth == Tree_reg_R[["finalModel"]][["tuneValue"]][["maxdepth"]]]))
+
+tree_grid_bin = expand.grid(maxdepth=3:9)
+Tree_bin_R = train(x=binarna[,binarna_X], y=binarna[,binarna_Y], tuneGrid=tree_grid_bin, method='rpart2', metric='Accuracy', trControl=cv_R)
+Tree_bin_R_Wynik = Tree_bin_R$results
+print(paste("Drzewo w R - bin: Max Depth = ", Tree_bin_R[["finalModel"]][["tuneValue"]][["maxdepth"]], " | Accuracy = " , Tree_bin_R_Wynik$Accuracy[Tree_bin_R_Wynik$maxdepth == Tree_bin_R[["finalModel"]][["tuneValue"]][["maxdepth"]]]))
+
+tree_grid_multi = expand.grid(maxdepth=3:9)
+Tree_multi_R = train(x=wieloklasowa[,wieloklasowa_X], y=wieloklasowa[,wieloklasowa_Y], tuneGrid=tree_grid_multi, method='rpart2', metric='Accuracy', trControl=cv_R)
+Tree_multi_R_Wynik = Tree_multi_R$results
+print(paste("Drzewo w R - multi: Max Depth = ", Tree_multi_R[["finalModel"]][["tuneValue"]][["maxdepth"]], " | Accuracy = " , Tree_multi_R_Wynik$Accuracy[Tree_multi_R_Wynik$maxdepth == Tree_multi_R[["finalModel"]][["tuneValue"]][["maxdepth"]]]))
+
+
+
+# KNN
+
+regresja_R <- regresja
+regresja_R[,1] <- as.numeric(regresja_R[,1])
+knn_grid_reg = expand.grid(k=2:15)
+KNN_reg_R = train(x=regresja_R[,regresja_X], y=regresja_R[,regresja_Y], tuneGrid=knn_grid_reg, method='knn', metric='MAE', trControl=cv_R)
+KNN_reg_R_Wynik = KNN_reg_R$results
+print(paste("KNN w R - reg: k = ", KNN_reg_R$finalModel$k, " | MAE = " ,KNN_reg_R_Wynik$MAE[KNN_reg_R_Wynik$k == KNN_reg_R$finalModel$k]))
+
+binarna_R <- binarna
+binarna_R[,7] <- as.numeric(as.character(as.numeric(binarna_R[,7])))
+knn_grid_bin = expand.grid(k=2:15)
+KNN_bin_R = train(x=binarna_R[,binarna_X], y=binarna_R[,binarna_Y], tuneGrid=knn_grid_bin, method='knn', metric='Accuracy', trControl=cv_R)
+KNN_bin_R_Wynik = KNN_bin_R$results
+print(paste("KNN w R - bin: k = ", KNN_bin_R$finalModel$k, " | Jakosc = " ,KNN_bin_R_Wynik$Accuracy[KNN_bin_R_Wynik$k == KNN_bin_R$finalModel$k]))
+
+knn_grid_multi = expand.grid(k=2:15)
+KNN_multi_R = train(x=wieloklasowa[,wieloklasowa_X], y=wieloklasowa[,wieloklasowa_Y], tuneGrid=knn_grid_multi, method='knn', metric='Accuracy', trControl=cv_R)
+KNN_multi_R_Wynik = KNN_multi_R$results
+print(paste("KNN w R - multi: k = ", KNN_multi_R$finalModel$k, " | Jakosc = " ,KNN_multi_R_Wynik$Accuracy[KNN_multi_R_Wynik$k == KNN_multi_R$finalModel$k]))
+
+
+
+
+
+VS_KNN_Reg <- data.frame(k = c(1:15))
+VS_KNN_Reg <- merge(VS_KNN_Reg, KNN_reg_CrossValid_gr[,c('k', "MAET")], by = 'k')
+VS_KNN_Reg <- merge(VS_KNN_Reg, KNN_reg_CrossValid_gr[,c('k', "MAEW")], by = 'k')
+VS_KNN_Reg <- merge(VS_KNN_Reg, KNN_reg_R_Wynik[,c('k', "MAE")], by = 'k')
+print(VS_KNN_Reg)
+
+ggplot(VS_KNN_Reg , aes(x=k)) + geom_line(aes(y = MAET, color='green'), size=1, ) + geom_line(aes(y = MAEW, color='blue'), size=1, ) + geom_line(aes(y = MAE, color='red'), size=1,) +
+  labs(title='KNN - Regresja: MAE od k', x='k', y='MAE') + scale_color_discrete(name = "Implementacja:", labels = c("Wlasna (W)", "Wlasna (T)","Biblioteka R")) + theme(legend.position = "bottom")
+
+
+VS_KNN_Bin <- data.frame(k = c(1:15))
+VS_KNN_Bin <- merge(VS_KNN_Bin, KNN_bin_CrossValid_gr[,c('k', "JakoscT")], by = 'k')
+VS_KNN_Bin <- merge(VS_KNN_Bin, KNN_bin_CrossValid_gr[,c('k', "JakoscW")], by = 'k')
+VS_KNN_Bin <- merge(VS_KNN_Bin, KNN_bin_R_Wynik[,c('k', "Accuracy")], by = 'k')
+print(VS_KNN_Bin)
+
+ggplot(VS_KNN_Bin , aes(x=k)) + geom_line(aes(y = JakoscT, color='green'), size=1, ) + geom_line(aes(y = JakoscW, color='blue'), size=1, ) + geom_line(aes(y = Accuracy, color='red'), size=1,) +
+  labs(title='KNN - Klas. Binarna: Jakosc od k', x='k', y='Jakosc') + scale_color_discrete(name = "Implementacja:", labels = c("Wlasna (W)", "Wlasna (T)", "Biblioteka R")) + theme(legend.position = "bottom")
+
+
+VS_KNN_Multi <- data.frame(k = c(1:15))
+VS_KNN_Multi <- merge(VS_KNN_Multi, KNN_multi_CrossValid_gr[,c('k', "JakoscT")], by = 'k')
+VS_KNN_Multi <- merge(VS_KNN_Multi, KNN_multi_CrossValid_gr[,c('k', "JakoscW")], by = 'k')
+VS_KNN_Multi <- merge(VS_KNN_Multi, KNN_multi_R_Wynik[,c('k', "Accuracy")], by = 'k')
+print(VS_KNN_Multi)
+
+ggplot(VS_KNN_Multi , aes(x=k)) + geom_line(aes(y = JakoscT, color='green'), size=1, ) + geom_line(aes(y = JakoscW, color='blue'), size=1, ) + geom_line(aes(y = Accuracy, color='red'), size=1,) +
+  labs(title='KNN - Klas. Wieloklasowa: Jakosc od k', x='k', y='Jakosc') + scale_color_discrete(name = "Implementacja:", labels = c("Wlasna (W)", "Wlasna (T)","Biblioteka R")) + theme(legend.position = "bottom")
+
+
+
+
+
+
+
+
 
 
 
