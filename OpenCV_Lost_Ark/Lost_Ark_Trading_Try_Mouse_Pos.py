4c5d4a36267b128dbf523d25eef08dc739027976
Szymon Baczyński <simon.baczynski@gmail.com>
2022-03-12 15:00:33 +0100

    Mini Project - Lost Ark - Mouse Position - zebrana tablica -> SHARD Items + price - każde osobno

--- OpenCV_Lost_Ark/Lost_Ark_Trading_Try_Mouse_Pos.py
+++ OpenCV_Lost_Ark/Lost_Ark_Trading_Try_Mouse_Pos.py
@@ -19,6 +19,7 @@ sleep(2)
 print("Move mouse to right-bottom corner of items")
 sleep(3)
 Gold_items_right_bottom = pyautogui.position()
+print("OKEY")
 
 Gold_items_find_screen = pyautogui.screenshot(region = (Gold_items_left_top[0], Gold_items_left_top[1], Gold_items_right_bottom[0] - Gold_items_left_top[0], Gold_items_right_bottom[1] - Gold_items_left_top[1]))
 Gold_items_find_screen_cv = cv2.cvtColor(np.array(Gold_items_find_screen), cv2.COLOR_RGB2BGR)
@@ -37,30 +38,57 @@ for item in Gold_Items_List:
 print(Gold_Items_List_new)
 
 sleep(2)
-# GOLD - Recent Price
 
-print("Move mouse to left-top corner of Avg. Day Price")
+# GOLD - Lowest Price
+
+print("Move mouse to left-top corner of GOLD - Lowest Price")
 sleep(3)
-Gold_recent_price_left_top = pyautogui.position()
+Gold_lowest_price_left_top = pyautogui.position()
 print("OKEY")
 sleep(2)
-print("Move mouse to right-bottom corner of Avg. Day Price")
+print("Move mouse to right-bottom corner of GOLD - Lowest Price")
 sleep(3)
-Gold_recent_price_right_bottom = pyautogui.position()
+Gold_lowest_price_right_bottom = pyautogui.position()
+print("OKEY")
 
-Gold_recent_price_find_screen = pyautogui.screenshot(region = (Gold_recent_price_left_top[0], Gold_recent_price_left_top[1], Gold_recent_price_right_bottom[0] - Gold_recent_price_left_top[0], Gold_recent_price_right_bottom[1] - Gold_recent_price_left_top[1]))
-Gold_recent_price_find_screen_cv = cv2.cvtColor(np.array(Gold_recent_price_find_screen), cv2.COLOR_RGB2BGR)
+Gold_lowest_price_find_screen = pyautogui.screenshot(region = (Gold_lowest_price_left_top[0], Gold_lowest_price_left_top[1], Gold_lowest_price_right_bottom[0] - Gold_lowest_price_left_top[0], Gold_lowest_price_right_bottom[1] - Gold_lowest_price_left_top[1]))
+Gold_lowest_price_find_screen_cv = cv2.cvtColor(np.array(Gold_lowest_price_find_screen), cv2.COLOR_RGB2BGR)
 
 # show found area with gold trading window
-# cv2.imshow("image", Gold_recent_price_find_screen_cv)
+# cv2.imshow("image", Gold_lowest_price_find_screen_cv)
 # cv2.waitKey(0)
 
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-Gold_recent_price_table = pytesseract.image_to_string(cv2.cvtColor(Gold_recent_price_find_screen_cv, cv2.COLOR_RGB2GRAY) )
-Gold_recent_price_List = Gold_recent_price_table.split("\n")
-Gold_recent_price_List = [x for x in Gold_recent_price_List if x.strip()]
-print(Gold_recent_price_List)
+Gold_lowest_price_table = pytesseract.image_to_string(cv2.cvtColor(Gold_lowest_price_find_screen_cv, cv2.COLOR_RGB2GRAY) )
+Gold_lowest_price_List = Gold_lowest_price_table.split("\n")
+Gold_lowest_price_List = [x for x in Gold_lowest_price_List if x.strip()]
+print(Gold_lowest_price_List)
 
 
-TABLE = pandas.DataFrame({"Items":Gold_Items_List_new, "Recent Gold":Gold_recent_price_List})
-print(TABLE)
+#TABLE = pandas.DataFrame({"Items":Gold_Items_List_new, "Lowest Gold":Gold_lowest_price_List})
+#print(TABLE)
+
+
+
+# SHARD ITEMS
+
+# Item - 1
+print("Move mouse to left-top corner of SHARD - Item #1")
+sleep(3)
+Gold_lowest_price_left_top = pyautogui.position()
+print("OKEY")
+sleep(2)
+print("Move mouse to right-bottom corner of SHARD - Item #1")
+sleep(3)
+Gold_lowest_price_right_bottom = pyautogui.position()
+print("OKEY")
+
+Gold_lowest_price_find_screen = pyautogui.screenshot(region = (Gold_lowest_price_left_top[0], Gold_lowest_price_left_top[1], Gold_lowest_price_right_bottom[0] - Gold_lowest_price_left_top[0], Gold_lowest_price_right_bottom[1] - Gold_lowest_price_left_top[1]))
+Gold_lowest_price_find_screen_cv = cv2.cvtColor(np.array(Gold_lowest_price_find_screen), cv2.COLOR_RGB2BGR)
+
+# show found area with gold trading window
+# cv2.imshow("image", Gold_lowest_price_find_screen_cv)
+# cv2.waitKey(0)
+
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+Gold_lowest_price_table = pytesseract.image_to_string(cv2.cvtColor(Gold_lowest_price_find_screen_cv, cv2.COLOR_RGB2GRAY) )
