ac598ca1430765222f59a80293185b45f4b1740f
Szymon Baczyński <simon.baczynski@gmail.com>
2022-03-23 14:41:56 +0100

    Mini Project - Lost Ark - Mouse Position - make Python env and compile to EXE

--- OpenCV_Lost_Ark/Lost_Ark_Trading_Try_Mouse_Pos.py
+++ OpenCV_Lost_Ark/Lost_Ark_Trading_Try_Mouse_Pos.py
@@ -1,23 +1,23 @@
 from numpy import array
-from cv2 import imread, imshow, waitKey, cvtColor, COLOR_RGB2BGR, COLOR_RGB2GRAY
+from cv2 import imread, imwrite, imshow, waitKey, cvtColor, COLOR_RGB2BGR, COLOR_RGB2GRAY
 import pytesseract 
 from pyautogui import position, screenshot
-# import pandas
+
 
 ### TODO: ZROBIC PLIK KONFIGURACYJNY
 
 def Region_to_Read(what_string):
     print("Move mouse to left-top corner of: " + what_string)
     input("And press ENTER")
-    position_left_top = pyautogui.position()
+    position_left_top = position()
     print("OKEY")
     print("Move mouse to right-bottom corner of: " + what_string)
     input("And press ENTER")
-    position_right_bottom = pyautogui.position()
+    position_right_bottom = position()
     print("OKEY")
 
-    position_find_screen = pyautogui.screenshot(region = (position_left_top[0], position_left_top[1], position_right_bottom[0] - position_left_top[0], position_right_bottom[1] - position_left_top[1]))
-    position_find_screen_cv = cv2.cvtColor(numpy.array(position_find_screen), cv2.COLOR_RGB2BGR)
+    position_find_screen = screenshot(region = (position_left_top[0], position_left_top[1], position_right_bottom[0] - position_left_top[0], position_right_bottom[1] - position_left_top[1]))
+    position_find_screen_cv = cvtColor(array(position_find_screen), COLOR_RGB2BGR)
 
     return(position_find_screen_cv)
 
@@ -28,18 +28,18 @@ def Show_and_Save(position_find_screen_cv, what_string, flag_show, flag_save):
     filename = what_string + "_PY_script.png"
 
     if flag_save:
-        cv2.imwrite(filename, position_find_screen_cv)
+        imwrite(filename, position_find_screen_cv)
 
     if flag_show:
-        cv2.imshow(filename, position_find_screen_cv)
-        cv2.waitKey(0)
+        imshow(filename, position_find_screen_cv)
+        waitKey(0)
 
 
 
 def Region_to_Text(what_string, flag_show = 1, flag_save = 1):
     position = Region_to_Read(what_string)
     Show_and_Save(position, what_string, flag_show, flag_save)
-    found_text = pytesseract.image_to_string(cv2.cvtColor(position, cv2.COLOR_RGB2GRAY) )
+    found_text = pytesseract.image_to_string(cvtColor(position, COLOR_RGB2GRAY) )
     found_text = found_text.split("\n")
     return found_text
 
@@ -59,7 +59,7 @@ def print_menu():
 # set Tesseract-OCR -> EXE directory
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-
+print("HEllo")
 gold_items = ''
 gold_items_price = ''
 
